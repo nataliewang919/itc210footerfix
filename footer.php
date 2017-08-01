0ef10ed0f4c7929f879f2e4116c1485a24cdecfa
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-07-31 19:48:58 -0700

    footer fix

---
+++ footer.php
@@ -0,0 +1,29 @@
+</div>
+	<div id="footer">
+        <div class="footertop">
+        <h3>ALWAYS APPRECIATE OUR PARTNERS</h3>
+        <img class="partners" src="<?php bloginfo('template_directory'); ?>/images/rover.png">
+		<img class="partners" src="<?php bloginfo('template_directory'); ?>/images/fred_meyer.png">
+        <img class="partners" id="last" src="<?php bloginfo('template_directory'); ?>/images/badge.png">
+        </div>
+		<div id="left">
+            <h3>Desiderate Rescue</h3>
+			<p>Sign up for news from Desiderata Rescue</p>
+			<img src="<?php bloginfo('template_directory'); ?>/emailsignup-fpo.png">
+		</div>
+		<div id="right">
+		<p>From unwanted to love?</p>	
+		<img class="icon" src="<?php bloginfo('template_directory'); ?>/images/dog.png">
+		<img class="icon" src="<?php bloginfo('template_directory'); ?>/images/instagram.png">
+        <img class="icon" src="<?php bloginfo('template_directory'); ?>/images/facebook.png">
+        <p>Phone:206.777.7777</p>
+        <p>desideratarescue@gmail.com</p>
+	</div>	
+        <div class="footerbottom">
+		<p>&copy All Rights Reserved to Desiderata Rescue</p>
+        <small>front-page.php</small>
+        </div>
+	</div>
+
+</body>
+</html>
